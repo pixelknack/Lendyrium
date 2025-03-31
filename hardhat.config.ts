6277773b1e1de3ca1020248d8388718f2baf723e
PixelKnackCodeHaven <ghost202444@proton.me>
2025-03-31 13:34:32 +0000

    dsds

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -10,8 +10,8 @@ export default {
       allowUnlimitedContractSize: true,
     },
     testnet: {
-      url: 'https://296.rpc.thirdweb.com/d391b93f5f62d9c15f67142e43841acc',
-      accounts: ["0xf76c1d3173b95df92480c690d227d30cf2eab6150b5cf0018b71f68df8e7cf96"],
+      url: 'https://296.rpc.thirdweb.com',
+      accounts: ["your-key-goes-here"],
       chainId: 296,
    }
   },
